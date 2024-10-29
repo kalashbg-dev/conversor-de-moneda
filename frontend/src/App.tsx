72ed54cc64e9f7f841c7bca77598556ccaadaf94
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-29 19:24:18 -0400

    feat: implemente react router with NextUI for ease of use

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { NextUIProvider } from '@nextui-org/react'
+import { useNavigate, useHref, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <h1>Currency Converter</h1>
-    </>
+    // NextUIProvider is a wrapper for all the components
+    <NextUIProvider navigate={useNavigate} useHref={useHref}>
+      {/* Routes for the app */}
+      <Routes>
+        <Route path="/" element={<h1>Currency Converter</h1>} />
+      </Routes>
+    </NextUIProvider>
   )
 }
 
