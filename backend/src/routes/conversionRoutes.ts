fa4165ec3285564111e25f1277e6290ea654240e
RichardFelic <felricharde42@gmail.com>
2024-10-30 21:38:30 -0400

    refactor: ajustar rutas de estructura y auth/role

--- backend/src/routes/conversionRoutes.ts
+++ backend/src/routes/conversionRoutes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
 import Conversion from "../models/Conversion";
 import { conversionSchema } from "../validators/conversionValidator";
-import { logConversion } from "../controllers/conversionController";
+import { getConversionHistory, logConversion } from "../controllers/conversionController";
 import { validateRequest } from "../middleware/validateRequest";
+import { roleMiddleware } from "../middleware/roleMiddleware";
+import { Roles } from "../constants/roles";
 
 const router = Router();
 
+// Rutas de conversion
 router.post("/convert", authMiddleware, validateRequest(conversionSchema), logConversion);
+
+// Ruta para obtener el historial de conversiones (solo para ADMIN)
+router.use(authMiddleware, roleMiddleware([Roles.ADMIN]));
+router.get("/", authMiddleware, getConversionHistory);
+
 export default router;
