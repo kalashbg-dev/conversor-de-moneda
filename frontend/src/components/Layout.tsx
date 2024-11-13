f48b9b8738b58f8c8193dd6a3ecc81445dafb327
RichardFelic <felricharde42@gmail.com>
2024-11-12 20:35:00 -0400

    feat: arreglo del front e integracion currencies a Home

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,12 +4,12 @@ import Navbar from './Navbar';
 interface LayoutProps {
   children: ReactNode;
 }
-
+// container mx-auto px-4 py-8
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="h-full bg-[#FFFFFF] dark:bg-gray-900">
       <Navbar />
-      <main className="container mx-auto px-4 py-8">
+      <main className=" mx-auto">
         {children}
       </main>
     </div>
