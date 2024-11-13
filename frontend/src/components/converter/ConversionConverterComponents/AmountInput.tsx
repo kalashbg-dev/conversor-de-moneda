64338e37a61b8fb75a1de66c02cb18364fed2b0f
Jislein <correo@correo.com>
2024-11-13 05:27:39 -0400

    Replaced NextUI component implementation with html tags and added some new Props to the interface declaration

--- frontend/src/components/converter/ConversionConverterComponents/AmountInput.tsx
+++ frontend/src/components/converter/ConversionConverterComponents/AmountInput.tsx
@@ -1,28 +1,37 @@
-import { Input } from "@nextui-org/react";
+// import { Input } from "@nextui-org/react";
+
 
 interface AmountInputProps {
-  value: string;
+  type: string;
+  label: string;
+  value: string | undefined;
   onChange: (value: string) => void;
+  // This is to disable editting on the 'Converted to' input.
+  readOnly?: boolean;
 }
 
-export function AmountInput({ value, onChange }: AmountInputProps) {
+export function AmountInput({ type, label, value, onChange, readOnly }: AmountInputProps) {
   return (
-    <Input
-      type="number"
-      placeholder="Amount"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      min={0}
-      size="lg"
-      startContent={
-        <div className="pointer-events-none flex items-center">
-          <span className="text-default-400">$</span>
-        </div>
-      }
-      classNames={{
-        input: "text-medium",
-        inputWrapper: "h-12",
-      }}
-    />
+    <div className="amount-input text-sm">
+      <label>{label}</label>
+      <input type={type} value={value} onChange={(e) => onChange(e.target.value)} min={0} readOnly={readOnly}/>
+    </div>
+    // <Input
+    //   type="number"
+    //   placeholder="Amount"
+    //   value={value}
+    //   onChange={(e) => onChange(e.target.value)}
+    //   min={0}
+    //   size="lg"
+    //   startContent={
+    //     <div className="pointer-events-none flex items-center">
+    //       <span className="text-default-400">$</span>
+    //     </div>
+    //   }
+    //   classNames={{
+    //     input: "text-medium",
+    //     inputWrapper: "h-12",
+    //   }}
+    // />
   );
 }
